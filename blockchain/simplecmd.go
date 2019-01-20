0fd5b04a4e967b27da1b022a743a6b970f25ba84
Aashaka Shah <aashaka96@gmail.com>
2019-01-20 12:54:16 -0600

    Coin-transfer chaincode

---
+++ blockchain/simplecmd.go
@@ -0,0 +1,142 @@
+package blockchain
+
+import (
+	"fmt"
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+	"time"
+)
+
+// InvokeOpen
+func (setup *FabricSetup) InvokeOpen(account string, value string) (string, error) {
+
+	// Prepare arguments
+	var args []string
+	args = append(args, "invoke")
+	args = append(args, "open")
+	args = append(args, account)
+	args = append(args, value)
+
+	eventID := "eventOpen"
+
+	// Add data that will be visible in the proposal, like a description of the invoke request
+	transientDataMap := make(map[string][]byte)
+	transientDataMap["result"] = []byte("Transient data in open")
+
+	reg, notifier, err := setup.event.RegisterChaincodeEvent(setup.ChainCodeID, eventID)
+	if err != nil {
+		return "", err
+	}
+	defer setup.event.Unregister(reg)
+
+	// Create a request (proposal) and send it
+	response, err := setup.client.Execute(channel.Request{ChaincodeID: setup.ChainCodeID, Fcn: args[0], Args: [][]byte{[]byte(args[1]), []byte(args[2]), []byte(args[3])}, TransientMap: transientDataMap})
+	if err != nil {
+		return "", fmt.Errorf("failed to open account: %v", err)
+	}
+
+	// Wait for the result of the submission
+	select {
+	case ccEvent := <-notifier:
+		fmt.Printf("Received CC event: %s\n", ccEvent)
+	case <-time.After(time.Second * 20):
+		return "", fmt.Errorf("did NOT receive CC event for eventId(%s)", eventID)
+	}
+
+	return string(response.TransactionID), nil
+}
+
+// InvokeQuery
+func (setup *FabricSetup) InvokeQuery(value string) (string, error) {
+
+	// Prepare arguments
+	var args []string
+	args = append(args, "invoke")
+	args = append(args, "query")
+	args = append(args, value)
+
+	// Create a request (proposal) and send it
+	response, err := setup.client.Execute(channel.Request{ChaincodeID: setup.ChainCodeID, Fcn: args[0], Args: [][]byte{[]byte(args[1]), []byte(args[2])} })
+	if err != nil {
+		return "", fmt.Errorf("failed to open account: %v", err)
+	}
+
+	return string(response.TransactionID), nil
+}
+
+// InvokeOpen
+func (setup *FabricSetup) InvokeTransfer(sender string, receiver string, value string) (string, error) {
+
+	// Prepare arguments
+	var args []string
+	args = append(args, "invoke")
+	args = append(args, "transfer")
+	args = append(args, sender)
+	args = append(args, receiver)
+	args = append(args, value)
+
+	eventID := "eventTransfer"
+
+	// Add data that will be visible in the proposal, like a description of the invoke request
+	transientDataMap := make(map[string][]byte)
+	transientDataMap["result"] = []byte("Transient data in transfer")
+
+	reg, notifier, err := setup.event.RegisterChaincodeEvent(setup.ChainCodeID, eventID)
+	if err != nil {
+		return "", err
+	}
+	defer setup.event.Unregister(reg)
+
+	// Create a request (proposal) and send it
+	response, err := setup.client.Execute(channel.Request{ChaincodeID: setup.ChainCodeID, Fcn: args[0], Args: [][]byte{[]byte(args[1]), []byte(args[2]), []byte(args[3]), []byte(args[4])}, TransientMap: transientDataMap})
+	if err != nil {
+		return "", fmt.Errorf("failed to open account: %v", err)
+	}
+
+	// Wait for the result of the submission
+	select {
+	case ccEvent := <-notifier:
+		fmt.Printf("Received CC event: %s\n", ccEvent)
+	case <-time.After(time.Second * 20):
+		return "", fmt.Errorf("did NOT receive CC event for eventId(%s)", eventID)
+	}
+
+	return string(response.TransactionID), nil
+}
+
+// InvokeDelete
+func (setup *FabricSetup) InvokeDelete(value string) (string, error) {
+
+	// Prepare arguments
+	var args []string
+	args = append(args, "invoke")
+	args = append(args, "delete")
+	args = append(args, value)
+
+	eventID := "eventDelete"
+
+	// Add data that will be visible in the proposal, like a description of the invoke request
+	transientDataMap := make(map[string][]byte)
+	transientDataMap["result"] = []byte("Transient data in delete")
+
+	reg, notifier, err := setup.event.RegisterChaincodeEvent(setup.ChainCodeID, eventID)
+	if err != nil {
+		return "", err
+	}
+	defer setup.event.Unregister(reg)
+
+	// Create a request (proposal) and send it
+	response, err := setup.client.Execute(channel.Request{ChaincodeID: setup.ChainCodeID, Fcn: args[0], Args: [][]byte{[]byte(args[1]), []byte(args[2])}, TransientMap: transientDataMap})
+	if err != nil {
+		return "", fmt.Errorf("failed to open account: %v", err)
+	}
+
+	// Wait for the result of the submission
+	select {
+	case ccEvent := <-notifier:
+		fmt.Printf("Received CC event: %s\n", ccEvent)
+	case <-time.After(time.Second * 20):
+		return "", fmt.Errorf("did NOT receive CC event for eventId(%s)", eventID)
+	}
+
+	return string(response.TransactionID), nil
+}
