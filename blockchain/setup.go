3169025aed9f7f8a976fb38e2cce82ed8be95c3c
Aashaka Shah <aashaka96@gmail.com>
2019-01-19 19:23:56 -0600

    Solves 'WARN No available peer groups'

--- blockchain/setup.go
+++ blockchain/setup.go
@@ -108,7 +108,7 @@ func (setup *FabricSetup) InstallAndInstantiateCC() error {
 	fmt.Println("Chaincode installed")
 
 	// Set up chaincode policy
-	ccPolicy := cauthdsl.SignedByAnyMember([]string{"org1.example.com"})
+	ccPolicy := cauthdsl.SignedByAnyMember([]string{"Org1MSP"})
 
 	resp, err := setup.admin.InstantiateCC(setup.ChannelID, resmgmt.InstantiateCCRequest{Name: setup.ChainCodeID, Path: setup.ChaincodeGoPath, Version: "0", Args: [][]byte{[]byte("init")}, Policy: ccPolicy})
 	if err != nil || resp.TransactionID == "" {
